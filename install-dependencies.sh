8457ad88390fcc26af5c3ecba5da721e3c1488b4
nikox94 <nikola.yuroukov@gmail.com>
2018-08-27 00:31:56 +1000

    Install Kubectl in TravisCI

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.11.2/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Elixir deps
 mix local.rebar --force # for Elixir 1.3.0 and up
 mix local.hex --force
 mix deps.get
