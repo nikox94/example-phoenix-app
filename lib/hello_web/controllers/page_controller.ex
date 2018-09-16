165c08a6db8b71dcac8e33145a816ce7cb3c9cfa
Nikola Yurukov <nikola.yuroukov@gmail.com>
2018-09-16 21:57:31 +1000

    Add spacious pipe

--- lib/hello_web/controllers/page_controller.ex
+++ lib/hello_web/controllers/page_controller.ex
@@ -12,6 +12,6 @@ defmodule HelloWeb.PageController do
 
   def list(conn, _params) do
     all = Hello.Repo.all(Hello.User)
-    render conn, "all.html", all: all |> Enum.map(fn x -> x.name end)
+    render conn, "all.html", all: all |> Enum.map(fn x -> x.name end) |> Enum.join(" ")
   end
 end
